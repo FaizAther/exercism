202a9315d72f26d961fb76a974d026dd134644fd
FaizAther <faizather@protonmail.com>
2021-03-22 18:12:11 +1000

    what even

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,23 @@
 module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n = if n < 1 || n > 64 then Nothing else Just $ square' n
+  where
+    square' n' = if n' > 1 then 2 * square'(n'-1) else 1
+
+total' :: [Maybe Integer]
+total' = map square [1..64]
+
+maybeAdd :: Maybe Integer -> Maybe Integer -> Maybe Integer
+maybeAdd Nothing _ = Nothing
+maybeAdd _ Nothing = Nothing
+maybeAdd (Just n) (Just m) = Just $ n + m
+
+total'' :: Maybe Integer
+total'' = foldl maybeAdd (Just 0) total'
 
 total :: Integer
-total = error "You need to implement this function."
+total = do
+  case total'' of
+    Just x -> x
+    Nothing -> -1 
