fe4c90d14f837bf033add35f472c213a424c9132
FaizAther <faizather@protonmail.com>
2021-03-25 20:44:40 +1000

    looks good

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,8 +1,14 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum $ [ y | y <- ls, anyFac y ]
+sumOfMultiples factors limit = foldl' someF 0 ls
   where
+    foldl' _ b []     = b
+    foldl' f b (x:xs) = let b' = (b `f` x)
+                        in seq b' foldl' f b' xs
     ls = [1..limit-1]
-    anyFac n = any (isFac n) factors
+    someF i j = i + anyFac j
+    anyFac n
+      | any (isFac n) factors = n
+      | otherwise             = 0
     isFac p q = q /= 0 && rem p q == 0
