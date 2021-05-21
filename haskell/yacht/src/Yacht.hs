ae27dd82eef4ac16d433790b740f567e8bb37ba8
FaizAther <faizather@protonmail.com>
2021-05-22 02:01:24 +1000

    yatch

--- haskell/yacht/src/Yacht.hs
+++ haskell/yacht/src/Yacht.hs
@@ -26,14 +26,15 @@ keepN (l:ls)  n
 
 just4 :: [[Int]] -> Int
 just4 ls
-  | length ls == 2 =  check4 ls
+  | length ls == 2 = check4 ls
+  | length ls == 1 = check4 ls
   | otherwise = 0
 
 check4 :: [[Int]] -> Int
 check4 [] = 0
 check4 (x:xs) =
-  if length x == 4
-  then sum x
+  if length x >= 4
+  then if length x == 5 then sum x - head x else sum x
   else check4 xs
 
 
@@ -45,12 +46,26 @@ yacht Fours xs = foldl (addN 4) 0 xs
 yacht Fives xs = foldl (addN 5) 0 xs
 yacht Sixes xs = foldl (addN 6) 0 xs
 yacht Choice xs = sum xs
-yacht FullHouse xs =  let (j:k:js) = foldl keepN [] xs in
-                        if (length (j:k:js) == 2) && (length j /= 1 && length j /= 4)
+yacht FullHouse xs =  let (j:js) = foldl keepN [] xs in
+                        if length (j:js) == 2 && length j /= 1 && length j /= 4
                         then sum xs
                         else 0
 yacht FourOfAKind xs =  let js = foldl keepN [] xs
                         in just4 js
 yacht Yacht [] = 0
 yacht Yacht (x:xs) = if foldl (\b a -> b && a == x) True xs then 50 else 0
-yacht _ xs = if length (foldl keepN [] xs) == 5 then 30 else 0
+yacht LittleStraight xs = let xs' = qsort xs 
+                          in if length (foldl keepN [] xs) == 5 && head xs' == 1 && last xs' == 5 then 30 else 0
+yacht BigStraight xs = let xs' = qsort xs 
+                       in if length (foldl keepN [] xs) == 5 && head xs' == 2 && last xs' == 6 then 30 else 0
+
+
+qsort :: [Int] -> [Int]
+qsort []     = []
+qsort (x:xs) = qsort lower ++
+               [pivot] ++
+               qsort upper
+  where
+    pivot = x
+    lower = [y | y <- xs, y <= x]
+    upper = [y | y <- xs, y >  x]
